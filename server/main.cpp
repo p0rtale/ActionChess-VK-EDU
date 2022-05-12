78f7b42ed8fc2f4c94ff5aeed8f22f31bc5b3827
p0rtale <petryaev_1984@mail.ru>
2022-05-12 23:59:22 +0300

    removed tabs in main

--- server/main.cpp
+++ server/main.cpp
@@ -8,28 +8,28 @@
 #include "Server.hpp"
 
 int main() {
-	std::shared_ptr<boost::asio::io_context> ioContext(std::make_shared<boost::asio::io_context>());
+    std::shared_ptr<boost::asio::io_context> ioContext(std::make_shared<boost::asio::io_context>());
 
-	Server server(ioContext, 8080);
-	server.run();
+    Server server(ioContext, 8080);
+    server.run();
 
-	std::vector<std::thread> threads;
-	for (int i = 0; i < 5; ++i) {
-		threads.emplace_back([ioContext]() {
-			for (;;) {
-				try {
-					ioContext->run();
-					break;
-				} catch (const std::exception& e) {
-					std::cerr << "ERROR: " << e.what() << '\n';
-				}
-			}
-		});
-	}
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back([ioContext]() {
+            for (;;) {
+                try {
+                    ioContext->run();
+                    break;
+                } catch (const std::exception& e) {
+                    std::cerr << "ERROR: " << e.what() << '\n';
+                }
+            }
+        });
+    }
 
-	for (auto& thread: threads) {
-		thread.join();
-	}
+    for (auto& thread: threads) {
+        thread.join();
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
